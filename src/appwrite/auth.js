6d7ba90b2dd88ee2c4d4b61b323f673e8a925ceb
CHETAN K.P <chetanrocks56@gmail.com>
2024-05-18 15:52:34 +0530

    configured Auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService{
+    client = new Client()
+    account
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            userAccount = await this.account.create(
+                ID.unique(), 
+                email, 
+                password,
+                name
+            )
+            if (userAccount) {
+                // call login after creating successfully
+                this.login({email, password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login ({email, password}){
+        try {
+            return await this.account.createEmailSession(
+                email,
+                password
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null
+    }
+}
+
+const authService = new AuthService()
+export default authService;
