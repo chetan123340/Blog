80337ba51047aecea8606aed741212e3e0e45a9a
CHETAN K.P <chetanrocks56@gmail.com>
2024-05-21 23:51:36 +0530

    forwardRef and common components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     const authStatus = useSelector(state=>state.auth.status)
     const navigate = useNavigate()
 
-    const naItems = [
+    const navItems = [
         {
             name: "Home",
             slug: "/",
@@ -36,7 +36,28 @@ function Header() {
     ]
 
     return ( <header className="py-3 shadow bg-gray-500">
-        
+        <Container>
+            <nav className="flex">
+                <div className="mr-4">
+                    <Link to="/">
+                        <Logo width="70px" />
+                    </Link>
+                </div>
+                <ul className="ml-auto">
+                    {navItems.map((item)=>
+                    item.active? (
+                        <li key={item.name}>
+                            <button onClick={navigate(item.slug)} className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">{item.name}</button>
+                        </li>
+                    ):null)}
+                    {authStatus && (
+                        <li>
+                            <LogoutBtn />
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </Container>
     </header> );
 }
 
